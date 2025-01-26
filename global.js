2f436271303aa7677ac1a771981e61b88f454dc4
r4tangUCSD <166660878+r4tangUCSD@users.noreply.github.com>
2025-01-25 21:10:14 -0800

    Added dark mode

--- global.js
+++ global.js
@@ -17,14 +17,56 @@ let pages = [
   { url: '', title: 'Home'},
   { url: 'projects/', title: 'Projects'},
   { url: 'contact/', title: 'Contact'},
-  { url: 'resume/', title: 'Resume'}
+  { url: 'resume/', title: 'Resume'},
+  { url: 'https://github.com/r4tangUCSD', title: 'GitHub'}
 ];
 
 let nav = document.createElement('nav');
 document.body.prepend(nav)
 
+const ARE_WE_HOME = document.documentElement.classList.contains('home');
+
 for (let p of pages) {
   let url = p.url;
   let title = p.title;
-  nav.insertAdjacentHTML('beforeend', `<a href="${url}">${title}</a>`);
+
+  if (!ARE_WE_HOME && !url.startsWith('http')) {
+    url = '../' + url;
+  }
+
+  let a = document.createElement('a');
+  a.href = url;
+  a.textContent = title;
+  if (a.host === location.host && a.pathname === location.pathname) {
+    a.classList.add('current');
+  }
+  if (a.host !== location.host) {
+    a.target = "_blank";
+  }
+
+  nav.append(a);
 }
+
+if ('colorScheme' in localStorage) {
+  document.documentElement.style.setProperty('color-scheme', localStorage.colorScheme);
+}
+
+document.body.insertAdjacentHTML(
+  'afterbegin',
+  `
+	<label class="color-scheme">
+		Theme:
+		<select>
+			<option>Automatic</option>
+      <option>Light</option>
+      <option>Dark</option>
+		</select>
+	</label>`
+);
+
+let select = document.querySelector("select");
+select.addEventListener('input', function (event) {
+  console.log('color scheme changed to', event.target.value);
+  document.documentElement.style.setProperty('color-scheme', event.target.value);
+  localStorage.colorScheme = event.target.value;
+});
