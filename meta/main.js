a0c77b445e18b9c35ff5436b780c745d4862f4b9
r4tangUCSD <166660878+r4tangUCSD@users.noreply.github.com>
2025-02-14 19:27:31 -0800

    index.html

--- meta/main.js
+++ meta/main.js
@@ -1,21 +1,21 @@
 let data = [];
 
 async function loadData() {
-  data = await d3.csv('loc.csv');
-  console.log(data);
+    data = await d3.csv('loc.csv', (row) => ({
+        ...row,
+        line: Number(row.line), // or just +row.line
+        depth: Number(row.depth),
+        length: Number(row.length),
+        date: new Date(row.date + 'T00:00' + row.timezone),
+        datetime: new Date(row.datetime),
+    }));
 }
 
+console.log(data)
+
 document.addEventListener('DOMContentLoaded', async () => {
-  await loadData();
+    await loadData();
 });
 
-async function loadData() {
-    data = await d3.csv('loc.csv', (row) => ({
-      ...row,
-      line: Number(row.line), // or just +row.line
-      depth: Number(row.depth),
-      length: Number(row.length),
-      date: new Date(row.date + 'T00:00' + row.timezone),
-      datetime: new Date(row.datetime),
-    }));
-  }
+let commits = d3.groups(data, (d) => d.commit);
+console.log(commits)
